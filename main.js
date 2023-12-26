9e6ff1536444fe54bfbd4fddfafdd1e765a3ef9b
Violetta357 <pampidyy@mail.ru>
2023-12-26 21:35:37 +0300

    3 задание

--- main.js
+++ main.js
@@ -9,5 +9,10 @@ alert(a);
 
 // 2 задание 
 
-let year = 2007
+let year = 2007;
 alert(`год выпуска Iphone ${year}`)
+
+// 3 задание 
+
+let nameJS = (`Брендан Эйх`);
+alert(`создатель JavaScript - ${nameJS}`)
