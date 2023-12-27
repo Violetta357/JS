324a0e75dec44f563335ac568ccb18862e3448b4
Violetta357 <pampidyy@mail.ru>
2023-12-27 18:39:10 +0300

    1 задание

--- hw2.3/main.js
+++ hw2.3/main.js
@@ -1,2 +1,11 @@
+// задание 1
 
+let password = 'пароль';
+let user = prompt('Введите пароль');
+
+if (password === user) {
+    alert ('Пароль введен верно')
+} else {
+    alert ('Пароль введен неправильно')
+}
 
