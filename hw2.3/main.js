43ff5290a9386d87366230bcef7e76756dff04b6
Violetta357 <pampidyy@mail.ru>
2023-12-27 18:49:37 +0300

    2 задание

--- hw2.3/main.js
+++ hw2.3/main.js
@@ -9,3 +9,12 @@ if (password === user) {
     alert ('Пароль введен неправильно')
 }
 
+// задание 2
+
+let c = 7;
+
+if (c > 0 && c < 10) {
+    console.log('верно');
+} else {
+    console.log('неверно');
+}
