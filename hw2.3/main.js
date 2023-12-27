c507f93fa40f9bc670cb6c94a8d8311ebb8fffa7
Violetta357 <pampidyy@mail.ru>
2023-12-27 19:33:54 +0300

    все раскомментировано

--- hw2.3/main.js
+++ hw2.3/main.js
@@ -1,40 +1,40 @@
-// // задание 1
+// задание 1
 
-// let password = 'пароль';
-// let user = prompt('Введите пароль');
+let password = 'пароль';
+let user = prompt('Введите пароль');
 
-// if (password === user) {
-//     alert ('Пароль введен верно')
-// } else {
-//     alert ('Пароль введен неправильно')
-// }
+if (password === user) {
+    alert ('Пароль введен верно')
+} else {
+    alert ('Пароль введен неправильно')
+}
 
-// // задание 2
+// задание 2
 
-// let c = 7;
+let c = 7;
 
-// if (c > 0 && c < 10) {
-//     console.log('верно');
-// } else {
-//     console.log('неверно');
-// }
+if (c > 0 && c < 10) {
+    console.log('верно');
+} else {
+    console.log('неверно');
+}
 
-// // задание 3
+// задание 3
 
-// let d = 150;
-// let e = 99;
+let d = 150;
+let e = 99;
 
-// if (d > 100 || e > 100) {
-//     console.log('верно');
-// } else {
-//     console.log('неверно');
-// }
+if (d > 100 || e > 100) {
+    console.log('верно');
+} else {
+    console.log('неверно');
+}
 
-// // задание 4
+// задание 4
 
-// let a = '2';
-// let b = '3';
-// alert(Number(a) + Number(b))
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b))
 
 // задание 5
 
@@ -65,3 +65,5 @@ switch (monthNumber) {
     alert('Такого месяца не существует')
     break;
 }
+
+
