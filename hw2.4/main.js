cc9db66f2f0bb05f5fc1e46344475519939227e9
Violetta357 <pampidyy@mail.ru>
2023-12-28 18:03:36 +0300

    3 задание

--- hw2.4/main.js
+++ hw2.4/main.js
@@ -1,15 +1,23 @@
 // задание 1
 
 let i = 0;
-while(i < 2){
-  console.log('привет');
-  i++
+while (i < 2) {
+  console.log("привет");
+  i++;
 }
 
 // задание 2
 
 let r = 1;
-while(r <= 5){
+while (r <= 5) {
   console.log(r);
-  r++
+  r++;
+}
+
+// задание 3
+
+let x = 7;
+while (x <= 22) {
+  console.log(x);
+  x++;
 }
