e4071cb3b79922a04be76ca654483ea52ccbe1e1
Violetta357 <pampidyy@mail.ru>
2024-01-11 18:20:08 +0300

    2 задание

--- hw2.5/main.js
+++ hw2.5/main.js
@@ -5,3 +5,11 @@ function min (a,b ) {
   // return Math.min(a,b) //второй вариант
 }
  console.log(min(4,8));
+
+//  задание 2
+
+function num (number) {
+  return number % 2 == 0 ? 'Число четное' : 'Число нечетное'
+  }
+  console.log(num(4));
+  console.log(num(7));
