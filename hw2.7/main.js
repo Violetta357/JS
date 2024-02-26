caffbe7ba71545edaf1005b9ddf7e685d6ead1b3
Violetta357 <pampidyy@mail.ru>
2024-02-26 18:15:03 +0300

    2 задание

--- hw2.7/main.js
+++ hw2.7/main.js
@@ -2,3 +2,21 @@
 let str = "js"
 str = str.toUpperCase();
 console.log(str);
+
+// задание 2
+function searchStart(arr, str){
+  const result = [];
+
+  for(let i = 0; i < arr.length; i ++){
+    const item = arr[i].toLowerCase();
+    if (item.startsWith(str.toLowerCase())){
+      result.push(arr[i])
+    }
+  }
+  return result;
+}
+
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+
