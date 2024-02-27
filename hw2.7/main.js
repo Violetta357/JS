4a706ada46ce773eb4b96af2e136a56508367a68
Violetta357 <pampidyy@mail.ru>
2024-02-27 16:00:13 +0300

    3 задание

--- hw2.7/main.js
+++ hw2.7/main.js
@@ -1,22 +1,28 @@
 // задание 1
-let str = "js"
+let str = "js";
 str = str.toUpperCase();
 console.log(str);
 
 // задание 2
-function searchStart(arr, str){
+function searchStart(arr, str) {
   const result = [];
 
-  for(let i = 0; i < arr.length; i ++){
+  for (let i = 0; i < arr.length; i++) {
     const item = arr[i].toLowerCase();
-    if (item.startsWith(str.toLowerCase())){
-      result.push(arr[i])
+    if (item.startsWith(str.toLowerCase())) {
+      result.push(arr[i]);
     }
   }
   return result;
 }
 
-console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
-console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
-console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+console.log(searchStart(["Кошка", "Кит", "Комар", "Носорог"], "ко"));
+console.log(searchStart(["яблоко", "груша", "гриб", "огурец"], "гру"));
+console.log(searchStart(["Дом", "Банк", "Больница", "Театр"], "Кино"));
 
+// задание 3
+let num = 32.58884;
+let down = Math.floor(num);
+let up = Math.ceil(num);
+let near = Math.round(num);
+console.log(down, up, near);
