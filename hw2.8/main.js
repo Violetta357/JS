6933a5bfab6c0017ad11c15fc315c8e4e331c939
Violetta357 <pampidyy@mail.ru>
2024-03-04 17:41:28 +0300

    2 задание

--- hw2.8/main.js
+++ hw2.8/main.js
@@ -1,11 +1,48 @@
 // задание 1
 
 const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
- ];
- 
- 
- console.log(people.sort((a,b) => a.age - b.age));
+  { name: "Глеб", age: 29 },
+  { name: "Анна", age: 17 },
+  { name: "Олег", age: 7 },
+  { name: "Оксана", age: 47 },
+];
+
+console.log(people.sort((a, b) => a.age - b.age));
+
+//  задание 2
+
+function map(arr, ruleFunction) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(ruleFunction(arr[i]));
+  }
+  return result;
+}
+
+function filter(arr, ruleFunction) {
+  const mapResult = map(arr, ruleFunction);
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (mapResult[i]) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+function isPositive(num) {
+  return num > 0;
+}
+function isMale(person) {
+  return person.gender === "male";
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const peoples = [
+  { name: "Глеб", gender: "male" },
+  { name: "Анна", gender: "female" },
+  { name: "Олег", gender: "male" },
+  { name: "Оксана", gender: "female" },
+];
+
+console.log(filter(peoples, isMale));
